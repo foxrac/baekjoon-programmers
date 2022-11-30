0af16594045b68e6a32df100ee0f99138a24a37e
foxrac <52904947+foxrac@users.noreply.github.com>
2022-11-30 15:25:53 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/lv4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -0,0 +1,9 @@
+select a.ANIMAL_ID,a.ANIMAL_TYPE,a.NAME
+from ANIMAL_INS a
+left join ANIMAL_OUTS b
+on a.ANIMAL_ID=b.ANIMAL_ID
+where (a.SEX_UPON_INTAKE like 'Intact%'
+and b.SEX_UPON_OUTCOME like 'Neutered%')
+or( a.SEX_UPON_INTAKE like 'Intact%'
+and b.SEX_UPON_OUTCOME like 'Spayed%')
+order by a.ANIMAL_ID
