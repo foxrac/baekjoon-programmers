236e30645d27c5969e76674a825d1cfaed607bee
foxrac <52904947+foxrac@users.noreply.github.com>
2023-01-12 14:15:10 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 1.59 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String my_string, int n) {
+        String answer = "";
+        String[] a = my_string.split("");
+        for(int i = 0 ; i < a.length ;i++){
+            for(int j = 0 ; j < n ; j++ ){
+                answer += a[i];
+            }
+        }
+        return answer;
+    }
+}
